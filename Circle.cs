2c1b53e83d692298ad4a35cd3784495972964c4f
eempc <evc484@gmail.com>
2019-03-27 16:12:23 +0000

    ListView and classes

---
+++ Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewListView {
+    class Circle {
+        public double radius { get; set; }
+        public const double pi = Math.PI;
+        public double area { get; set; }
+        public string type { get; set; }
+
+        public Circle(double radius) {
+            this.radius = radius;
+            this.area = Area();
+            this.type = "Circle";
+        }
+
+        public virtual double Area() {
+            return radius * radius * pi;
+        }
+
+        public double Diameter() {
+            return radius * 2;
+        }
+
+        public double Circumference() {
+            return radius * 2 * pi;
+        }
+    }
+
+    class Sphere : Circle {
+        public double volume { get; set; }
+
+        public Sphere(double radius) : base (radius) {
+            this.radius = radius;
+            this.area = Area();
+            this.volume = Volume();
+            this.type = "Sphere";
+        }
+
+        public override double Area() {
+            return base.Area() * 4;
+        }
+
+        public double Volume() {
+            return base.Area() * radius * 4 / 3;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewListView {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e) {
+            Circle myCircle = new Circle(5);
+            testTextBox.AppendText($"Radius: {myCircle.radius}; Area: {myCircle.area}" + Environment.NewLine);
+            Sphere mySphere = new Sphere(8);
+            testTextBox.AppendText($"Radius: {mySphere.radius}; Area: {mySphere.area}; Volume: {mySphere.volume}");
+        }
+
+        private void Ralph_Click(object sender, RoutedEventArgs e) {
+            myListView0.Items.Add(new Circle(5));
+            myListView0.Items.Add(new Circle(8));
+            myListView0.Items.Add(new Sphere(5));
+            myListView0.Items.Add(new Sphere(8));
+        }
+    }    
+}
